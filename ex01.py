d8b5e4db4a629581e0412eaac189dcaa59bb3e99
noah <22294@ecam.be>
2025-04-03 21:08:25 +0200

    good

--- ex01.py
+++ ex01.py
@@ -1,1 +1,9 @@
-"print"
+import re 
+
+text = "noahmanono2443@gmail.com"
+pattern = r'^[a-zA-Z0-9._]+@[a-z]+.[a-z]{2,}$'
+if re.match(pattern,text):
+    print("c'est ok")
+
+else:
+    print("c'est ko")
