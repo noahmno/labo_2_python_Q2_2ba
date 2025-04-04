db005a6537b6187fbb8015126936f88d8c633ea2
noah <22294@ecam.be>
2025-04-04 11:50:22 +0200

    ga

--- ex01.py
+++ ex01.py
@@ -1,9 +1,14 @@
 import re 
 
 text = "noahmanono2443@gmail.com"
-pattern = r'^[a-zA-Z0-9._]+@[a-z]+.[a-z]{2,}$'
-if re.match(pattern,text):
-    print("c'est ok")
+#pattern = r'^[a-zA-Z0-9._]+@[a-z]+.[a-z]{2,}$'
+#if re.match(pattern,text):
+ #   print("c'est ok")
 
-else:
-    print("c'est ko")
+#else:
+#    print("c'est ko")
+
+pattern = r"^[a-z]+$"
+p = re.compile(pattern, re.IGNORECASE)
+print(p.match("bogoss"))
+print(p.match("LaTeX") )
